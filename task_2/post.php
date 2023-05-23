e3b6a28216bf52c9bec65cd1d51d6e204b284863
MZ-87 <zsmile@mail.ru>
2023-05-23 16:42:58 +0300

    change php script

--- task_2/post.php
+++ task_2/post.php
@@ -1,19 +1,7 @@
 <?php
-// Получаем значения переменных из пришедших данных
-$index = $_POST['index'];
-$country = $_POST['country'];
-$region = $_POST['region'];
-$city = $_POST['city'];
-$street = $_POST['street'];
-$home_number = $_POST['home_number'];
+header("Content-Type: application/json");
 
-// Формируем сообщение для отправки, в нём мы соберём всё, что ввели в форме
-$mes = "Индекс: $index \nСтрана: $country \nОбласть: $region \nГород: $city \nУлица: $street \nДом: $home_number";
-// Пытаемся отправить письмо по заданному адресу
-// Если нужно, чтобы письма всё время уходили на ваш адрес — замените первую переменную $email на свой адрес электронной почты
-$send = mail($email, $mes, "Content-type:text/plain; charset = UTF-8\r\nFrom:$email");
-// Если отправка прошла успешно — так и пишем
-if ($send == 'true') {echo "Сообщение отправлено";}
-// Если письмо не ушло — выводим сообщение об ошибке
-else {echo "Ошибка, сообщение не отправлено";}
+$data = json_decode(file_get_contents("php://input"));
+
+return $response->withJson($data);
 ?>
